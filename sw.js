7b28bb034075f865aec2f3947e92848ce348f2f3
milenasferrari <milenasferrari@gmail.com>
2025-11-27 03:51:07 -0500

    Refactor service worker event listeners and cache

--- sw.js
+++ sw.js
@@ -1,27 +1,22 @@
-const CACHE_NAME = 'hinos-pwa-v1';
-const ASSETS = [
-  'hinos_app_checklist_pwa.html',
-  'manifest.webmanifest'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open("hinos-cache-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest",
+        "./sw.js",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
   );
 });
